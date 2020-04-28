87b95f711684f1faf1e02818a6c42ad46fe7fe20
Nikolay <n.shulyakov@gmail.com>
2020-04-28 16:19:40 +0300

    HomeWork_2 fixed #1

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -2,29 +2,9 @@
 #include <cstdlib>
 #include <iostream>
 
-#include "ip_filter_lib.h"
+#include "libs/ip_filter_lib.h"
 
-auto split(const std::string &str, const char& d)
-{
-    std::vector<std::string> r;
-
-    decltype(auto) stop = str.find_first_of(d);
-    decltype(stop) start = 0;
-    while(stop != std::string::npos)
-    {
-        r.push_back(str.substr(start, stop - start));
-        start = stop + 1;
-        stop = str.find_first_of(d, start);
-    }
-
-    r.push_back(str.substr(start));
-
-    return r;
-}
-
-
-
-void print_ip(const std::vector<std::vector<std::string> >& ip_pool)
+void print_ip(const std::vector<std::vector<unsigned char> >& ip_pool)
 {
     for(auto ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
     {
@@ -35,7 +15,7 @@ void print_ip(const std::vector<std::vector<std::string> >& ip_pool)
                 std::cout << ".";
 
             }
-            std::cout << *ip_part;
+            std::cout << +*ip_part;
         }
         std::cout << std::endl;
     }
@@ -45,90 +25,38 @@ int main()
 {
     try
     {
-        std::vector<std::vector<std::string> > ip_pool;
+        std::vector<std::vector<unsigned char> > ip_pool;
 
         for(std::string line; std::getline(std::cin, line);)
         {
             std::vector<std::string> v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
+            try{
+                ip_pool.emplace_back(convert_to_uchars(split(v.at(0), '.')));
+            }
+            catch(const std::exception &e)
+            {
+                std::cout << e.what() << "\n";
+            }
         }
 
-        // TODO reverse lexicographically sort
-        std::sort(ip_pool.begin(), ip_pool.end(), [](auto ip_1, auto ip_2){
+        std::sort(ip_pool.begin(), ip_pool.end(), [](auto& ip_1, auto& ip_2){
             for(decltype(ip_1.size()) i = 0; i<ip_1.size(); ++i)
-                if(std::atoi(ip_1.at(i).c_str()) != std::atoi(ip_2.at(i).c_str()))
-                    return std::atoi(ip_1.at(i).c_str()) > std::atoi(ip_2.at(i).c_str());
+                if(ip_1 != ip_2)
+                    return ip_1 > ip_2;
             return false;
         });
-        print_ip(ip_pool);
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
 
-        // TODO filter by first byte and output
-        // ip = filter(1)
+        print_ip(ip_pool);
+        
         auto ip_filter_1 = filter(ip_pool, 1);
         print_ip(ip_filter_1);
-
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
+        
         auto ip_filter_2 = filter(ip_pool, 46, 70);
         print_ip(ip_filter_2);
-
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-
-        // TODO filter by any byte and output
-        // ip = filter_any(46)
+        
         auto ip_filter_3 = filter_any(ip_pool, 46);
         print_ip(ip_filter_3);
-
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
+        
     }
     catch(const std::exception &e)
     {
